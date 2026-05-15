3205411c1ea68efd7aeed3e31277f8741ee612ad
deepak-s-git <deepak.s.workdesk@gmail.com>
2026-05-15 16:48:46 +0530

    Initial Commit

---
+++ Echo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Echo
+//
+//  Created by Deepak S on 15/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Deepak S on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

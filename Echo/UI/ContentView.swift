2fe88c4caba044129302fe3faaaf277601dba976
deepak-s-git <deepak.s.workdesk@gmail.com>
2026-06-16 22:14:37 +0530

    feat(ContentView): handle app launching state

--- Echo/UI/ContentView.swift
+++ Echo/UI/ContentView.swift
@@ -5,20 +5,34 @@ struct ContentView: View {
     @EnvironmentObject var permissionsManager: PermissionsManager
 
     var body: some View {
-        Group {
+        ZStack {
             switch appStore.state {
             case .launching:
                 LaunchView()
+                    .transition(.opacity)
             case .ready:
-                if appStore.isOnboardingPresented {
-                    OnboardingView()
-                } else if !permissionsManager.allGranted {
-                    PermissionsView()
-                } else {
-                    MainNavigationView()
+                ZStack {
+                    Color(red: 0.05, green: 0.05, blue: 0.055)
+                        .ignoresSafeArea()
+                    
+                    ZStack {
+                        if appStore.isOnboardingPresented {
+                            OnboardingView()
+                                .transition(.opacity.combined(with: .scale(scale: 0.98)))
+                        } else if !permissionsManager.allGranted {
+                            PermissionsView()
+                                .transition(.opacity.combined(with: .scale(scale: 1.02)))
+                        } else {
+                            MainNavigationView()
+                                .transition(.opacity)
+                        }
+                    }
+                    .animation(.spring(response: 0.7, dampingFraction: 0.82), value: appStore.isOnboardingPresented)
+                    .animation(.spring(response: 0.7, dampingFraction: 0.82), value: permissionsManager.allGranted)
                 }
             case .failed(let error):
                 ErrorView(error: error)
+                    .transition(.opacity)
             }
         }
         .frame(minWidth: 900, maxWidth: 900, minHeight: 620, maxHeight: 620)
